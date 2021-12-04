6150819471271e9c3f874d5c34716d3f5368e2cb
kaniket7209 <kaniket7209@gmail.com>
2021-12-04 23:49:53 +0530

    update

--- app.py
+++ app.py
@@ -1,45 +1,46 @@
-import  os
+import os
 from flask import Flask, render_template, request
 from flask_mail import Mail, Message
 from dotenv import load_dotenv
 
 load_dotenv()
 
-def create_app():
-    app = Flask(__name__)
 
-    app.config['DEBUG'] = True
-    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-    app.config['MAIL_PORT'] = 465
-    app.config['MAIL_USE_TLS'] = False
-    app.config['MAIL_USE_SSL'] = True
-    app.config['MAIL_USERNAME'] = os.environ.get("email")
-    app.config['MAIL_PASSWORD'] = os.environ.get("pass")
-    app.config['MAIL_DEFAULT_SENDER'] = os.environ.get("email")
-    app.config['MAIL_MAX_EMAILS'] = None
+app = Flask(__name__)
 
-    mail = Mail(app)
+app.config['DEBUG'] = True
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = os.environ.get("email")
+app.config['MAIL_PASSWORD'] = os.environ.get("pass")
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get("email")
+app.config['MAIL_MAX_EMAILS'] = None
+
+mail = Mail(app)
 
 
-    @app.route('/')
-    def index():
-        return render_template("home.html")
+@app.route('/')
+def index():
+    return render_template("home.html")
 
-    @app.route('/send_message', methods = ['GET', 'POST'])
-    def send_message():
-        if(request.method == 'POST'):
-            email = request.form['email']
-            subject = request.form['subject']
-            msg = request.form['message']
 
-            message = Message(subject, sender=os.environ.get("email"), recipients=[email])
+@app.route('/send_message', methods=['GET', 'POST'])
+def send_message():
+    if(request.method == 'POST'):
+        email = request.form['email']
+        subject = request.form['subject']
+        msg = request.form['message']
 
-            message.body = msg # .html will make it in html format 
-            with app.open_resource('hii.jpg') as hii:
-                message.attach('hii.jpg', 'image/jpg', hii.read())
-                
-            mail.send(message)
-            success = "Message sent"
-            return render_template("result.html",success=success)
+        message = Message(subject, sender=os.environ.get(
+            "email"), recipients=[email])
 
-    return app
+        message.body = msg  # .html will make it in html format
+
+        with app.open_resource('hii.jpg') as hii:
+            message.attach('hii.jpg', 'image/jpg', hii.read())
+
+        mail.send(message)
+        success = "Message sent"
+        return render_template("result.html", success=success)
